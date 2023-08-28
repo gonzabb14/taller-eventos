256bcfa232b5ca5702a68570c3aec7b4236cfd84
Gonzalo Maulella <gonzavv14@gmail.com>
2023-08-28 16:54:10 -0300

    Agregué que no se propague el click al div si pulso el button

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ let div = document.getElementsByTagName("div");
 div[0].addEventListener("click", function(e) {
     alert("Hola! Soy el div");
 });
+
+function saludar(event) {
+    event.stopPropagation();
+    alert("hola");
+}
